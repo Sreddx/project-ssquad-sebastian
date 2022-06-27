83e69676b2c73a163749e1b27dd4913a32d0a06b
Sreddx <rssebas08@outlook.com>
2022-06-27 02:17:11 +0000

    Added service restart to redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,9 +3,9 @@
 tmux kill-server || echo "No tmux server to kill"
 
 cd /home/project-ssquad-sebastian
-git fetch && git reset origin/main
-
+git fetch && git reset --hard  origin/main
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
 
-tmux new-session -d -s myportfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+#tmux new-session -d -s myportfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myporfolio.service
